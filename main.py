313495c856ff2d093980288985a998cfd3009e2b
joseh <hardmandez@hotmail.com>
2025-08-14 22:27:26 +0100

    Initial.

--- main.py
+++ main.py
@@ -7,13 +7,22 @@ money_machine = MoneyMachine()
 coffee_maker_menu = Menu()
 # coffee_maker_menu_item = MenuItem()
 choice = ""
-
+machine_on = True
 
 # Get users input.
+while machine_on:
 
-# choice = input(f"Make a drink selection {coffee_maker_menu.get_items()}.")
-while choice != coffee_maker_menu.find_drink(choice) or choice != "off" or choice != "report":
-    choice = input(f"Make a drink selection {coffee_maker_menu.get_items()}.")
+    choice = input(f"Make a drink selection {coffee_maker_menu.get_items()}.").lower()
 
-
+    #Check input
+    if choice == "off":
+        print("Powering off...")
+        machine_on = False
+    elif choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        coffee_maker_menu.find_drink(choice)
+        print("Thank you for choosing.")
+        payment = money_machine.make_payment()
 
